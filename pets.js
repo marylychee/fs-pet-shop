72bd730b6bad9bd50a962d382c0f0c9208c8edf2
Mary Lai <marylychee@gmail.com>
2017-02-22 13:36:08 -0800

    Add the read subcommand

--- pets.js
+++ pets.js
@@ -4,12 +4,22 @@ var fs = require('fs');
 var path = require('path');
 var petsPath = path.join(__dirname, 'pets.json');
 
-fs.readFile(petsPath, 'utf8', function(err, data){
-  if (err){
-    throw err;
-  }
 
-  var pets = JSON.parse(data);
+var node = path.basename(process.argv[0]);
+var file = path.basename(process.argv[1]);
+var cmd = process.argv[2];
 
-  console.log(pets); 
-})
+if (cmd === 'read') {
+  fs.readFile(petsPath, 'utf8', function(err, data) {
+    if (err) {
+      throw err;
+    }
+    var pets = JSON.parse(data);
+
+    console.log(pets);
+  })
+}
+else {
+  console.error(`Usage: ${node} ${file} read`);
+  process.exit(1);
+}
