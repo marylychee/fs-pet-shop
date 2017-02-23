6f3abd95e6503b69fe5b469349f07471ff894cbe
Mary Lai <marylychee@gmail.com>
2017-02-22 17:42:39 -0800

    Send all pet  records from the database with GET

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 'use strict'
 
+var fs = require('fs');
+var path = require('path');
+var petsPath = path.join(__dirname, 'pets.json');
+
 var express = require('express');
 var app = express();
 var port = process.env.PORT  || 8000;
 
 app.disable('x-powered-by');
 
-app.get('/guests', function(req, res) {
-  var guests = ['Mary', 'Don'];
-  res.send(guests);
+app.get('/pets', function(req, res) {
+  fs.readFile(petsPath, 'utf8', function(err, petsJSON) {
+    if (err) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+
+    var pets = JSON.parse(petsJSON);
+
+    res.send(pets);
+  });
 });
 
 app.use(function(req, res){
